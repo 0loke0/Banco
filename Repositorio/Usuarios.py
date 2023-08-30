f1370462ade0f7b514eeb29e117470e7a7965b1c
Jeison Fernandez <jeison.fernandez@sinco.com.co>
2023-08-29 22:20:01 -0500

    Avances eje 2

---
+++ Repositorio/Usuarios.py
@@ -0,0 +1,11 @@
+
+
+class Usuarios:
+    def __init__(self,identifiadorAnterios:int,numeroCedula:int,nombreCompleto:str) -> None:
+        self.identifiadorAnterios = identifiadorAnterios + 1,
+        self.numeroCedula = numeroCedula,
+        self.nombreCompleto = nombreCompleto
+
+    def MostrarUsuariosRegistrados():
+        print()
+
